8b47c34073dd7f81046e7f83945e7391c93fe5d4
margarevalo <margarevalo>
2024-03-03 23:46:08 +0800

    pageeee

--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -89,7 +89,16 @@ export default function Home() {
 
 
 return (
-<main style={{ alignItems: 'center', backgroundImage: `url('https://img.freepik.com/free-vector/gradient-rainbow-glitter-background_52683-97583.jpg?w=996&t=st=1709217399~exp=1709217999~hmac=ed155792ae65e4136fa554c2c26ae8a6079b2662fc42359fc0d75532b32e551f')`, backgroundPosition: 'center', backgroundSize: 'cover', display: 'flex', flexDirection: 'column', justifyContent: 'center', minHeight: '100vh',}}>
+<main style={{
+  alignItems: 'center',
+  display: 'flex',
+  justifyContent: 'center',
+  minHeight: '100vh',
+  flexDirection: 'column',
+  backgroundSize: 'cover',
+  backgroundImage: `url('https://img.freepik.com/free-vector/gradient-rainbow-glitter-background_52683-97583.jpg?w=996&t=st=1709217399~exp=1709217999~hmac=ed155792ae65e4136fa554c2c26ae8a6079b2662fc42359fc0d75532b32e551f')`,
+  backgroundPosition: 'center',
+}}>
   <button
     onClick={() => { connectWallet(); }}
     className="p-3 bg-green-200 text-white rounded"
@@ -99,27 +108,32 @@ return (
   <br></br>
 
   <div className="flex">
-    {['Mint', 'Stake'].map((action, index) => (
-      <div key={index} className={`flex-1 text-center bg-${index === 0 ? 'yellow' : 'pink'}-200 p-${index === 0 ? '6' : '4'} rounded-md flex flex-col justify-center items-center `}>
-        <br></br>
-        <form>
-          <label> Amount of {action}</label><br></br>
-          <input
-            type="text"
-            value={index === 0 ? mintingAmount || "" : stakingAmount || ""}
-            onChange={(e) => index === 0 ? handleMintAmountChange(e) : handleStakeAmountChange(e)}
-            style={{ color: "Black" }}
-          />
-        </form>
-        <br></br>
-        <button
-          onClick={() => index === 0 ? handleMint() : handleStake()}
-          className={`p-3 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 via-green-500 to-blue-500 via-indigo-500 to-purple-500 text-black rounded rounded`}
-        >
-          {action}
-        </button>
-      </div>
-    ))}
+    {['Mint', 'Stake'].map((action, index) => {
+      const bgColor = index === 0 ? 'yellow' : 'pink';
+      const paddingSize = index === 0 ? '6' : '4';
+
+      return (
+        <div key={index} className={`flex-1 text-center bg-${bgColor}-200 p-${paddingSize} rounded-md flex flex-col justify-center items-center `}>
+          <br></br>
+          <form>
+            <label> Amount of {action}</label><br></br>
+            <input
+              type="text"
+              value={index === 0 ? mintingAmount || "" : stakingAmount || ""}
+              onChange={(e) => index === 0 ? handleMintAmountChange(e) : handleStakeAmountChange(e)}
+              style={{ color: "Black" }}
+            />
+          </form>
+          <br></br>
+          <button
+            onClick={() => index === 0 ? handleMint() : handleStake()}
+            className={`p-3 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 via-green-500 to-blue-500 via-indigo-500 to-purple-500 text-black rounded rounded`}
+          >
+            {action}
+          </button>
+        </div>
+      );
+    })}
   </div>
 
   <div>
@@ -133,7 +147,5 @@ return (
     </button>
   </div>
 </main>
-
-
   );
 }
