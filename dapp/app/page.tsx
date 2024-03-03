4d7231a65d252b0edafdd5eeb2111296134193a8
margarevalo <margarevalo>
2024-03-03 23:39:37 +0800

    hehes

--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -5,22 +5,22 @@ import { useEffect, useState } from "react";
 import { getContract } from "../config";
 
 export default function Home() {
-  const [walletKey, setwalletKey] = useState("");
-  const [currentData, setcurrentData] = useState("");
+  const [walletKey, setWalletKey] = useState("");
+  const [currentData, setCurrentData] = useState("");
 
   const connectWallet = async () => {
     const { ethereum } = window as any;
     const accounts = await ethereum.request({
       method: "eth_requestAccounts",
     });
-    setwalletKey(accounts[0]);
+    setWalletKey(accounts[0]);
   };
-  //<Minting>
+
   const [mintingAmount, setMintingAmount] = useState<number>();
   const [submitted, setSubmitted] = useState(false);
   const [transactionHash, setTransactionHash] = useState("");
   
-  const mintCoin = async () => {
+  const handleMint = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
     const signer = await provider.getSigner();
@@ -35,7 +35,7 @@ export default function Home() {
       alert(`Minting failed: ${decodedError?.args}`);
     }
   };
-  const mintAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleMintAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
     if (!isNaN(Number(inputValue))) {
       setMintingAmount(Number(inputValue));
@@ -44,10 +44,9 @@ export default function Home() {
       setMintingAmount(0);
     }
   };
-  //</Minting>
- //<Staking>
+
   const [stakingAmount, setStakingAmount] = useState<number>();
-  const stakeCoin = async () => {
+  const handleStake = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
     const signer = await provider.getSigner();
@@ -59,10 +58,10 @@ export default function Home() {
       setTransactionHash(tx.hash);
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
-      alert(`Minting failed: ${decodedError?.args}`);
+      alert(`Staking failed: ${decodedError?.args}`);
     }
   };
-  const stakeAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleStakeAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
     if (!isNaN(Number(inputValue))) {
       setStakingAmount(Number(inputValue));
@@ -71,10 +70,8 @@ export default function Home() {
       setStakingAmount(0);
     }
   };
-  //</Staking>
- 
-  //<Withdraw>
-  const withdrawCoin = async () => {
+
+  const handleWithdraw = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
     const signer = await provider.getSigner();
@@ -86,82 +83,57 @@ export default function Home() {
       setTransactionHash(tx.hash);
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
-      alert(`Minting failed: ${decodedError?.args}`);
+      alert(`Withdrawal failed: ${decodedError?.args}`);
     }
   };
-  //</Withdraw>
+
+
 return (
-    <main   style={{
-      alignItems: 'center',
-      display: 'flex',
-      justifyContent: 'center',
-      minHeight: '100vh',
-      flexDirection: 'column',
-      backgroundSize: 'cover',
-      backgroundImage: `url('https://img.freepik.com/free-vector/gradient-rainbow-glitter-background_52683-97583.jpg?w=996&t=st=1709217399~exp=1709217999~hmac=ed155792ae65e4136fa554c2c26ae8a6079b2662fc42359fc0d75532b32e551f')`,
-      backgroundPosition: 'center',}}>
-          <button onClick={() => {connectWallet();}}
-        className="p-3 bg-green-200 text-white rounded"
-      >
-        {walletKey != "" ? walletKey : " Connect wallet"}
-      </button>
-      <br></br>\
-
-      <div className="flex">
-      <div className="flex-1 text-center bg-yellow-200 p-6 rounded-md flex flex-col justify-center items-center ">
-      <br></br>
-      <form>
-        <label> Amount of Mint</label><br></br>
-        </form>
-      <input
-        type="text"
-        value = {mintingAmount || ""}
-        onChange = {(e) => mintAmountChange(e)}
-        style={{color:"Black"}}
-      />
-       <br></br>
-      <button 
-        onClick={() => {mintCoin();}}
-        className="p-3 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 via-green-500 to-blue-500 via-indigo-500 to-purple-500 text-black rounded rounded"
-      >
-        {"Mint"}
-      </button> 
-      
-    </div>
-    <br></br>
-
-<div className="flex-1 text-center bg-pink-200 p-4 rounded-md flex flex-col justify-center items-center">
-<form>
-    <label> Amount of Stake </label><br></br>
-    </form>
-  <input
-    type="text"
-    value = {stakingAmount || ""}
-    onChange = {(e) => stakeAmountChange(e)}
-    style={{color:"Black"}}
-  />
-  <br></br>
- 
-  <button 
-    onClick={stakeCoin}
-    className="p-3 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 via-green-500 to-blue-500 via-indigo-500 to-purple-500 text-black rounded"
+<main style={{ alignItems: 'center', backgroundImage: `url('https://img.freepik.com/free-vector/gradient-rainbow-glitter-background_52683-97583.jpg?w=996&t=st=1709217399~exp=1709217999~hmac=ed155792ae65e4136fa554c2c26ae8a6079b2662fc42359fc0d75532b32e551f')`, backgroundPosition: 'center', backgroundSize: 'cover', display: 'flex', flexDirection: 'column', justifyContent: 'center', minHeight: '100vh',}}>
+  <button
+    onClick={() => { connectWallet(); }}
+    className="p-3 bg-green-200 text-white rounded"
   >
-    {"Stake"}
-  </button> 
-  </div>
-</div>
+    {walletKey !== "" ? walletKey : " Connect wallet"}
+  </button>
+  <br></br>
 
-<div>
-    <br></br>
-    <br></br>
-    <button 
-        onClick={withdrawCoin}
-        className="p-3 bg-blue-500 text-white rounded"
-      >
-        {"Withdraw"}
-      </button> 
+  <div className="flex">
+    {['Mint', 'Stake'].map((action, index) => (
+      <div key={index} className={`flex-1 text-center bg-${index === 0 ? 'yellow' : 'pink'}-200 p-${index === 0 ? '6' : '4'} rounded-md flex flex-col justify-center items-center `}>
+        <br></br>
+        <form>
+          <label> Amount of {action}</label><br></br>
+          <input
+            type="text"
+            value={index === 0 ? mintingAmount || "" : stakingAmount || ""}
+            onChange={(e) => index === 0 ? handleMintAmountChange(e) : handleStakeAmountChange(e)}
+            style={{ color: "Black" }}
+          />
+        </form>
+        <br></br>
+        <button
+          onClick={() => index === 0 ? handleMint() : handleStake()}
+          className={`p-3 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 via-green-500 to-blue-500 via-indigo-500 to-purple-500 text-black rounded rounded`}
+        >
+          {action}
+        </button>
       </div>
+    ))}
+  </div>
 
-    </main>
+  <div>
+    <br></br>
+    <br></br>
+    <button
+      onClick={() => { handleWithdraw(); }}
+      className="p-3 bg-blue-500 text-white rounded"
+    >
+      {"Withdraw"}
+    </button>
+  </div>
+</main>
+
+
   );
 }
